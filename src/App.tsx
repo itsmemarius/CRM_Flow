d5b4ea6f7b955bda354d617ad54affa1edcb070e
Marius Bejenuta <marius.bejenuta@gmail.com>
2026-02-02 01:23:06 +0200

    Fix: Re-uploading project from root directory

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { CRMDashboard } from './components/Dashboard/CRMDashboard'
+
+export function App() {
+  return (
+    <div className="min-h-screen bg-slate-100 text-slate-900 font-sans">
+       <CRMDashboard />
+    </div>
+  )
+}
